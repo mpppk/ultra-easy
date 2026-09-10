9eaef1606c396db875a189ce439a10f21faef6f9
niboshi <niboshiporipori@gmail.com>
2026-09-10 23:33:51 +0900

    feat: export approval domain model

---
+++ packages/approval-core/src/domain/index.ts
@@ -0,0 +1,6 @@
+export type * from "./action.ts";
+export type * from "./condition.ts";
+export type * from "./flow.ts";
+export type * from "./json.ts";
+export type * from "./policy.ts";
+export type * from "./principal.ts";
