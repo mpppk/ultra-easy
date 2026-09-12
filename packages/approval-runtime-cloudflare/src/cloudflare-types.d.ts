0461e16082dab67523179bb2b5d355600b9353e5
niboshi <niboshiporipori@gmail.com>
2026-09-13 06:32:15 +0900

    fix: Cloudflare EnvをWorkflow bindingsで拡張

--- packages/approval-runtime-cloudflare/src/cloudflare-types.d.ts
+++ packages/approval-runtime-cloudflare/src/cloudflare-types.d.ts
@@ -1,2 +1,14 @@
 /// <reference types="@cloudflare/workers-types" />
 /// <reference types="@cloudflare/vitest-plugin/types" />
+
+import type { ActionWorkflowEnv, ActionWorkflowParams } from "./workflow.ts";
+
+declare global {
+  namespace Cloudflare {
+    interface Env extends ActionWorkflowEnv {
+      ACTION_WORKFLOW: Workflow<ActionWorkflowParams>;
+    }
+  }
+}
+
+export {};
